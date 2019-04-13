80fe0fc9fe3738e0c04e630402d2d0fd8877dad1
Claudio E. de Oliveira <claudioed.oliveira@gmail.com>
2019-04-13 18:59:38 -0300

    first commit

---
+++ src/test/java/tech/claudioed/shipment/domain/NativeresourcesIT.java
@@ -0,0 +1,9 @@
+package tech.claudioed.shipment.domain;
+
+import io.quarkus.test.junit.SubstrateTest;
+
+@SubstrateTest
+public class NativeresourcesIT extends resourcesTest {
+
+    // Execute the same tests but in native mode.
+}
